2b90bc2f4055f8e643e387a07226145f907fe1f5
Avan Peltier <tex@csh.rit.edu>
2022-06-06 10:52:36 -0500

    Switch to javascript

---
+++ FOOTBALL.js
@@ -0,0 +1,18 @@
+const { InstallerProvider} = require('@slack/oauth');
+const { createEventAdapter } = require('@slack/events-api');
+const { createMessageAdapter } = require('@slack/interactive-messages')
+const YahooFantasy = require('yahoo-fantasy');
+
+
+const yf = new YahooFantasy(
+    process.env.APPLICATION_KEY,
+    process.env.APPLICATION_SECRET
+);
+const installer = new InstallerProvider({
+    clientId: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    stateSecret: 'current-state'
+});
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackEvents = createEventAdapter(slackSigningSecret);
+const slackInteractions = createMessageAdapter(slackSigningSecret);
