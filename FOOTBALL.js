871b8c2534a518e923803125cc8dc6deb8b16d7c
Avan Peltier <tex@csh.rit.edu>
2022-06-10 14:08:31 -0500

    Weird error

--- FOOTBALL.js
+++ FOOTBALL.js
@@ -1,5 +1,5 @@
-const  { InstallProvider, defaultCallbackFailure} = require('@slack/oauth');
 const  {App} = require('@slack/bolt');
+const {FileInstallationStore} = require('@slack/oauth')
 const { createEventAdapter } = require('@slack/events-api');
 const { createMessageAdapter } = require('@slack/interactive-messages')
 const YahooFantasy = require('yahoo-fantasy');
@@ -25,50 +25,10 @@ const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     socketMode: true,
     appToken: process.env.APP_TOKEN,
-    /*clientId: process.env.SLACK_CLIENT_ID,
+    clientId: process.env.SLACK_CLIENT_ID,
     clientSecret: process.env.SLACK_CLIENT_SECRET,
     stateSecret: 'state',
-    installationStore: {
-        storeInstallation: async (installation) => {
-          // Bolt will pass your handler an installation object
-          // Change the lines below so they save to your database
-          if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
-            // handle storing org-wide app installation
-            return await database.set(installation.enterprise.id, installation);
-          }
-          if (installation.team !== undefined) {
-            // single team app installation
-            return await database.set(installation.team.id, installation);
-          }
-          throw new Error('Failed saving installation data to installationStore');
-        },
-        fetchInstallation: async (installQuery) => {
-          // Bolt will pass your handler an installQuery object
-          // Change the lines below so they fetch from your database
-          if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-            // handle org wide app installation lookup
-            return await database.get(installQuery.enterpriseId);
-          }
-          if (installQuery.teamId !== undefined) {
-            // single team app installation lookup
-            return await database.get(installQuery.teamId);
-          }
-          throw new Error('Failed fetching installation');
-        },
-        deleteInstallation: async (installQuery) => {
-          // Bolt will pass your handler  an installQuery object
-          // Change the lines below so they delete from your database
-          if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-            // org wide app installation deletion
-            return await database.delete(installQuery.enterpriseId);
-          }
-          if (installQuery.teamId !== undefined) {
-            // single team app installation deletion
-            return await database.delete(installQuery.teamId);
-          }
-          throw new Error('Failed to delete installation');
-        },
-      },*/
+    installationStore: new FileInstallationStore()
 });
 
 app.message('hello', async ({message, say}) => {
