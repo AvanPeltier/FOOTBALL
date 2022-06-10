3e4ff819f79e970614c47ee833fcb59a4527f077
Avan Peltier <tex@csh.rit.edu>
2022-06-10 13:11:53 -0500

    I'm big dumb needed to change to Socket Mode dear Lord end me now

--- FOOTBALL.js
+++ FOOTBALL.js
@@ -1,11 +1,14 @@
 const  { InstallProvider, defaultCallbackFailure } = require('@slack/oauth');
+const  {App} = require('@slack/bolt');
 const { createEventAdapter } = require('@slack/events-api');
 const { createMessageAdapter } = require('@slack/interactive-messages')
 const YahooFantasy = require('yahoo-fantasy');
 const { createServer } = require('http');
+require('dotenv').config({path: 'SlackSecrets.env'})
+
 
 const port = process.env.PORT || 3000;
-const host = 'http://football-csh/FOOTBALL';
+//const host = 'localhost';
 /*const requestListener = function (req, res) {
     res.writeHead(200);
     res.end("Server")
@@ -16,10 +19,22 @@ const yf = new YahooFantasy(
     process.env.APPLICATION_KEY,
     process.env.APPLICATION_SECRET
 );
+
+const app = new App({
+    token: process.env.BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.APP_TOKEN
+});
+
+app.message('hello', async ({message, say}) => {
+    await say(`I'm alive, Howdy <@${message.user}>`);
+});
+/*
 const installer = new InstallProvider({
     clientId: process.env.SLACK_CLIENT_ID,
     clientSecret: process.env.SLACK_CLIENT_SECRET,
-    stateSecret: 'current-state'
+    stateSecret: process.env.STATE
 });
 const server = createServer((req, res) => {
     if (req.url === '/slack/oauth_redirect'){
@@ -27,31 +42,32 @@ const server = createServer((req, res) => {
     }
 });
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
-const slackEvents = createEventAdapter(slackSigningSecret, 
-);
+const slackEvents = createEventAdapter(slackSigningSecret);
 const slackInteractions = createMessageAdapter(slackSigningSecret);
-const eventServer = createServer(slackEvents.requestListener());
 
 
 slackEvents.on('message', (event) => {
     console.log(`Recieved a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
 
 });
-/*(async () => {
-    const interactServer = await slackInteractions.start(3001);
-    console.log(`Listening for events on ${interactServer.address().address}`);
- })().catch(() => {
-    console.log('Catch warning')
+(async () => {
+    const eventServer = await slackEvents.start(3000);
+    console.log(`Listening for events on ${eventServer.address().port}`);
+ })().catch ( (error) => {
+    console.log(`Error: ${error}`)
 });
 */
-/*
+
 (async () => {
-    
+    await app.start(3000)
+    console.log('Running')
 })().catch(() => {
     console.log('Catch warning')
 });
-*/
-eventServer.listen(port, host, () => {
+
+/*eventServer.listen(port, host, () => {
     console.log(`Listening for events on ${eventServer.address().address}`);
-});
+});*/
+/*
+server.listen(8080)*/
 console.log("Ran");
